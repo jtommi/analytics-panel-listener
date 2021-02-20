f54e26b022e65c3d6f15fc20a260f536f9f343a7
Thomas <johanns.thomas@gmail.com>
2021-02-20 13:46:55 +0000

    Proper error handling

--- controllers/handleEvent.js
+++ controllers/handleEvent.js
@@ -3,14 +3,15 @@ const DashboardEvent = require('../models/DashboardEvent');
 module.exports = async (req, res, next) => {
     switch (req.body['type']) {
         case 'start':
-            console.log("'start'-event received for UUID %s", req.body['uuid']);
+            console.log(req.body);
+            console.log("'%s'-event received for UUID %s", req.body['type'], req.body['uuid']);
             // Create a new event entry in the database
             const dashboardEvent = DashboardEvent({
                 _id: req.body['uuid'],
                 dashboardId: req.body['dashboard']['uid'],
                 user: req.body['user']['login'],
-                startDate: Date(req.body['time'] * 1000),
-                lastSeen: Date(req.body['time'] * 1000),
+                startDate: new Date(req.body['time'] * 1000),
+                lastSeen: new Date(req.body['time'] * 1000),
                 userSettings: {
                     lightTheme: req.body['user']['lightTheme'],
                     timezone: req.body['user']['timezone'],
@@ -20,8 +21,15 @@ module.exports = async (req, res, next) => {
             dashboardEvent.save()
                 .then(result => {
                     console.log(result)
-                    res.statusCode = 201;
-                    res.end();
+                    res.status(201).json({
+                        status: "success",
+                        data: {
+                            uuid: result._id,
+                            sessionDuration: 0,
+                            logged_time: result.lastSeen.getTime() / 1000
+                        },
+                        message: ""
+                    });
                 })
                 .catch(err => {
                     console.log(err)
@@ -31,42 +39,67 @@ module.exports = async (req, res, next) => {
                         error.statusCode = 422;
                         next(error);
                     }
+                    else {
+                        next(err);
+                    }
                 });
             break;
 
         case 'heartbeat':
-            console.log("'heartbeat'-event received for UUID %s", req.body['uuid']);
+        case 'end':
+            console.log("'%s'-event received for UUID %s", req.body['type'], req.body['uuid']);
             // Get the corresponding dashboard event
             var event = await DashboardEvent.findOne({ _id: req.body['uuid'] });
             // Set lastSeen and calculate session duration before updating the DB
-            event.lastSeen = Date(req.body['time'] * 1000);
-            event.sessionDuration = (event.lastSeen.getTime() - event.startDate.getTime()) / 1000;
-            event.save()
-                .then(result => {
-                    res.statusCode = 201;
-                    res.end();
-                });
-            break;
-
-        case 'end':
-            console.log("'end'-event received for UUID %s", req.body['uuid']);
-            // Get the corresponding dashboard event
-            var event = await DashboardEvent.findOne({ _id: req.body['uuid'] });
-            if (event.endDate) {
-                const error = Error("Session already ended");
-                error.statusCode = 422;
-                next(error);
+            if (event) {
+                event.lastSeen = new Date(req.body['time'] * 1000);
+                event.sessionDuration = (event.lastSeen.getTime() - event.startDate.getTime()) / 1000;
+                if (req.body['type'] === 'end') {
+                    if (event.endDate) {
+                        // Session already ended
+                        const error = Error("Session already ended");
+                        error.statusCode = 422;
+                        next(error);
+                    }
+                    event.endDate = event.lastSeen
+                }
             }
             else {
-                // Set endDate and calculate session duration before updating the DB
-                event.endDate = Date(req.body['time'] * 1000);
-                event.sessionDuration = (event.endDate.getTime() - event.startDate.getTime()) / 1000;
-                event.save()
-                    .then(result => {
-                        res.statusCode = 201;
-                        res.end();
-                    });
+                // If no event is found we create one
+                event = DashboardEvent({
+                    _id: req.body['uuid'],
+                    dashboardId: req.body['dashboard']['uid'],
+                    user: req.body['user']['login'],
+                    startDate: new Date(req.body['time'] * 1000),
+                    lastSeen: new Date(req.body['time'] * 1000),
+                    userSettings: {
+                        lightTheme: req.body['user']['lightTheme'],
+                        timezone: req.body['user']['timezone'],
+                        locale: req.body['user']['locale']
+                    },
+                    sessionDuration: 0
+                })
+                if (req.body['type'] === 'end') {
+                    event.endDate = event.lastSeen
+                }
             }
+            event.save()
+                .then(result => {
+                    console.log(result)
+                    res.status(201).json({
+                        status: "success",
+                        data: {
+                            uuid: result._id,
+                            logged_time: result.lastSeen.getTime() / 1000,
+                            sessionDuration: result.sessionDuration
+                        },
+                        message: ""
+                    });
+                })
+                .catch(err => {
+                    console.log(err)
+                    next(err);
+                });
             break;
 
         default:
--- server.js
+++ server.js
@@ -40,8 +40,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   res.statusCode = error.statusCode || 500;
   res.json({
-    error: {
-      message: error.message
-    }
+    status: "error",
+    message: error.message
   });
 });
